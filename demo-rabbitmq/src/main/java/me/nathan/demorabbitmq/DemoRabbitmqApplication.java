d21f5c431eb12577a9b0d66e42acb3729e1b4bcd
USER <user@AL01277083.local>
2020-10-07 11:13:24 +0900

    add kafka & rabbitmq

---
+++ demo-rabbitmq/src/main/java/me/nathan/demorabbitmq/DemoRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package me.nathan.demorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitmqApplication.class, args);
+	}
+
+}
---
+++ demo-rabbitmq/src/test/java/me/nathan/demorabbitmq/DemoRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nathan.demorabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
